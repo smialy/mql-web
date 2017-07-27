f37e4c00a8e308ced4091228bc106d3d658c8bcf
Smialy <p.smialkowski@gmail.com>
2017-07-27 22:42:34 +0200

    Improve transforms

--- app/models.py
+++ app/models.py
@@ -8,22 +8,38 @@ from mql.engines import psql
 
 class AclTransformer(NodeTransformer):
     def visit_SelectStatement(self, node):
-        acl = expression('s_owner = 1 AND s_unixperms = 256 OR s_group = 1 AND s_unixperms = 32 OR s_unixperms = 4')
+        acl = expression('s_owner = 1 AND s_unixperms = 256 OR s_group = 1 AND s_unixperms = 32 OR s_unixperms & 4 = 4')
         if node.where:
-            node.where = ast.LogicExpression(
+            node.where.condition = ast.LogicExpression(
                 'and',
                 acl,
-                node.where
+                node.where.condition
             )
         else:
-            node.where = acl
+            node.where = ast.SelectWhere(acl)
+
+        if node.results[0].is_wildcard():
+            node.results = [ast.SelectIdentifier("")]
+        else:
+            node.results = [item for item in node.results if not item.name.startswith('_')]
+
+        return node
+
+
+class LimitTransformer(NodeTransformer):
+    def visit_SelectStatement(self, node):
+        if not node.limit:
+            node.limit = ast.SelectLimit(100)
         return node
 
 
 async def create_engine(dsn):
     pool = await aiopg.create_pool(dsn)
     engine = psql.create_engine(pool)
-    # engine.add_transformer(AclTransformer())
+
+    engine.add_transformer(AclTransformer())
+    engine.add_transformer(LimitTransformer())
+
     schema = await engine.load_schema()
     return Mql(engine, schema)
     # data = await mql.execute('SELECT name FROM test WHERE id = 1')
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from aiohttp.hdrs import METH_POST
-from aiohttp.web import json_response
+from aiohttp.web import json_response, Response
 from aiohttp.web_exceptions import HTTPFound
 from aiohttp_jinja2 import template
 
@@ -15,5 +15,5 @@ async def index(request):
 
 async def query(request):
     mql = request.app['mql']
-    data = await mql.execute('SELECT id, name FROM test')
-    return json_response(data)
+    data = await mql.execute('SELECT id, name, _a FROM public.test ORDER BY name')
+    return Response(text=data, content_type='application/json')
